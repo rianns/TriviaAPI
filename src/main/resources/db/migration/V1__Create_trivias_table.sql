f05bfec07b6e260f549d6a5dacbb88ea040373e8
Rigene Salang <rigene.salang@gmail.com>
2023-07-31 16:57:35 +1000

    revert commit

--- src/main/resources/db/migration/V1__Create_trivias_table.sql
+++ src/main/resources/db/migration/V1__Create_trivias_table.sql
@@ -8,7 +8,7 @@ CREATE TABLE trivias (
     id INT AUTO_INCREMENT PRIMARY KEY,
     difficulty VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL,
-    question VARCHAR(999) NOT NULL,
+    question VARCHAR(1000) NOT NULL,
     correct_answer VARCHAR(255) NOT NULL,
     incorrect_answers VARCHAR(1000) NOT NULL
 );
