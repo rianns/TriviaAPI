e24f05852008b278056f5c32918266ad378514c5
Rigene Salang <rigene.salang@gmail.com>
2023-08-03 16:56:40 +1000

    added Game entity and setup

--- src/main/java/io/nology/triviaapi/game/Game.java
+++ src/main/java/io/nology/triviaapi/game/Game.java
@@ -1,10 +1,95 @@
 package io.nology.triviaapi.game;
 
+import java.util.Date;
+import java.util.List;
+
+import io.nology.triviaapi.trivia.TriviaDTO;
+
+// all about the game
 public class Game {
     Long id;
 
+    // this calls the trivia API to get trivia
     // TriviaDTO trivia;
+    private TriviaDTO trivia;
 
-    // answers
+    // user submitted answers
+    // change from front-end to get an object with key-value pair (question id,
+    // answer)
+    private List<String> userAnswers;
 
+    // user score
+    private int score;
+
+    // date played
+    private Date date;
+
+    // correct answers
+    private List<String> correctAnswers;
+
+    // if a question was failed or not
+    private boolean failed;
+
+    // TODO: #3 game history
+
+    // TODO: #2 retry past game
+
+    public Game() {
+    }
+
+    public Game(int score, Date date, List<String> correctAnswers, boolean failed) {
+        super();
+        this.score = score;
+        this.date = date;
+        this.correctAnswers = correctAnswers;
+        this.failed = failed;
+    }
+
+    public TriviaDTO getTrivia() {
+        return trivia;
+    }
+
+    public void setTrivia(TriviaDTO trivia) {
+        this.trivia = trivia;
+    }
+
+    public List<String> getUserAnswers() {
+        return userAnswers;
+    }
+
+    public void setUserAnswers(List<String> userAnswers) {
+        this.userAnswers = userAnswers;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<String> getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public void setCorrectAnswers(List<String> correctAnswers) {
+        this.correctAnswers = correctAnswers;
+    }
+
+    public boolean isFailed() {
+        return failed;
+    }
+
+    public void setFailed(boolean failed) {
+        this.failed = failed;
+    }
 }
--- src/main/java/io/nology/triviaapi/game/GameController.java
+++ src/main/java/io/nology/triviaapi/game/GameController.java
@@ -2,4 +2,13 @@ package io.nology.triviaapi.game;
 
 public class GameController {
 
+    // filter questions by failed
+
+    // have a filter to get all entries have correct = "false"
+
+    // Get trivias from db
+
+    // add correct answers in an array
+
+    //
 }
